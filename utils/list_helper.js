0ad9e09087b172b0b10daab451da52b687b16841
C. Yang <hello@cheryllium.com>
2023-07-23 12:55:18 -0400

    Exercises 4.3 - 4.7

---
+++ utils/list_helper.js
@@ -0,0 +1,65 @@
+var _ = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, v) => acc + v.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  let favoriteBlog = { likes: 0 }
+  console.log('favoriteBlog', favoriteBlog)
+  console.log('blogs', blogs)
+  for(const blog of blogs) {
+    console.log('blog', blog)
+    if(blog.likes >= favoriteBlog.likes) {
+      favoriteBlog = blog
+    }
+  }
+
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  }
+}
+
+const mostBlogs = (blogs) => {
+  const authors = _.uniq(blogs.map(b => b.author))
+  const authorCounts = authors.map(author => {
+    return {
+      author,
+      blogs: blogs.reduce((acc, v) => {
+        if(v.author == author) {
+          return acc + 1
+        }
+        return acc
+      }, 0)
+    }
+  })
+
+  return _.maxBy(authorCounts, a => a.blogs)
+}
+
+const mostLikes = (blogs) => {
+  const authors = _.uniq(blogs.map(b => b.author))
+  const authorCounts = authors.map(author => {
+    return {
+      author,
+      likes: blogs.reduce((acc, v) => {
+        if(v.author == author) {
+          return acc + v.likes
+        }
+        return acc
+      }, 0)
+    }
+  })
+
+  return _.maxBy(authorCounts, a => a.likes)  
+}
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
