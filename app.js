8b37f101801f1a0a4971f508b92ea4ec1eb469eb
C. Yang <hello@cheryllium.com>
2023-07-23 11:28:23 -0400

    Refactor application (exercise 4.2)

---
+++ app.js
@@ -0,0 +1,19 @@
+const config = require('./utils/config')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const Blog = require('./models/blog')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+const blogsRouter = require('./controllers/blog')
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
