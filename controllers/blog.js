79540c6e73175f165142fa2450d92d437f9aa42e
C. Yang <hello@cheryllium.com>
2023-07-26 16:42:55 -0400

    Exercise 4.19

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -12,8 +21,11 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end()
   }
 
-  const users = await User.find({})
-  const user = users[0]
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   
   const blog = request.body.likes
         ? new Blog({ ...request.body, user: user.id })
